bcb6cd57a805c691f442bf8b9fd8d9055b3059fd
hzshang <hzshang15@gmail.com>
2018-12-17 21:27:26 +0800

    add readme.me

--- testcase/test.java
+++ testcase/test.java
@@ -1,15 +1,5 @@
 class test{
 	public static void main(String[] a){
-		System.out.println(1+2*3+5);
+		System.out.println(1);
 	}
-}
-class BS{
-    public int begin(){
-        if(a){
-            a=a+2;
-        }else{
-            a=a+1;
-        }
-        return 1;
-    }
 }
