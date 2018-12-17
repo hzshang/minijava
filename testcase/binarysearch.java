05d8682db3f2aa2c70fda8ce9951f95ee66e0c98
eric <eric_liu@gmail.com>
2018-12-17 10:58:42 +0800

    passed all testcase

--- testcase/binarysearch.java
+++ testcase/binarysearch.java
@@ -1,6 +1,6 @@
 class BinarySearch{
     public static void main(String[] a){
-	System.out.println(new BS().Start());
+	System.out.println(new BS().Start(20));
     }
 }
 // This class contains an array of integers and
@@ -8,7 +8,7 @@ class BinarySearch{
 // using Binary Search
 
 class BS{
-    int number ;
+    int[] number ;
     int size ;
 
     // Invoke methods to initialize, print and search
@@ -16,23 +16,23 @@ class BS{
     public int Start(int sz){
 	int aux01 ;
 	int aux02 ;
-	aux01 = this.Init();
+	aux01 = this.Init(sz);
 	aux02 = this.Print();
-	if (this.Search()) System.out.println(1) ;
+	if (this.Search(8)) System.out.println(1) ;
 	else System.out.println(0) ;
-	if (this.Search()) System.out.println(1) ;
+	if (this.Search(19)) System.out.println(1) ;
 	else System.out.println(0) ;
-	if (this.Search()) System.out.println(1) ;
+	if (this.Search(20)) System.out.println(1) ;
 	else System.out.println(0) ;
-	if (this.Search()) System.out.println(1) ;
+	if (this.Search(21)) System.out.println(1) ;
 	else System.out.println(0) ;
-	if (this.Search()) System.out.println(1) ;
+	if (this.Search(37)) System.out.println(1) ;
 	else System.out.println(0) ;
-	if (this.Search()) System.out.println(1) ;
+	if (this.Search(38)) System.out.println(1) ;
 	else System.out.println(0) ;
-	if (this.Search()) System.out.println(1) ;
+	if (this.Search(39)) System.out.println(1) ;
 	else System.out.println(0) ;
-	if (this.Search()) System.out.println(1) ;
+	if (this.Search(50)) System.out.println(1) ;
 	else System.out.println(0) ;
 
 	return 999 ;
